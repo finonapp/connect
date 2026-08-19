7397f3d5de820fd7628ff9ea142a2580f2e4e717
eyubkh <eyub.kh@gmail.com>
2026-08-19 15:37:59 +0100

    Remove registry export and associated provider metadata for simplification

--- src/connections/registry.ts
+++
@@ -1,4 +0,0 @@
-import type { ProviderMetadata } from "../core/metadata";
-import { trading212Metadata } from "./trading212/metadata";
-
-export const providers: ProviderMetadata[] = [trading212Metadata];
